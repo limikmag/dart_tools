f913e2982105c8ff27acfe6556e2d6c4eab49dcd
limikmag <limikmag@gmail.com>
2019-03-17 15:21:30 +0100

    Create printing_variables.dart

---
+++ printing_variables.dart
@@ -0,0 +1,5 @@
+void main() {
+  List<String> shopList = ['pepper', 'orange', 'banana', 'lettuce'];
+  int bucketSize = shopList.length;
+  print('Our bucket have a size ${bucketSize} and we have inside: \n${shopList}');
+}
